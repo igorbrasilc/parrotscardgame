de659e8c490daae2f310a4771b46c5c1fc63f205
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-02-03 11:32:13 -0300

    versão dois, com o CSS Transition de virar cartas implementado (ainda tem problema de o efeito não obedecer nenhum efeito)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
     let qtdCartas = parseInt(prompt("Com quantas cartas você quer jogar? (números pares de 4 a 14)"));
+    let gifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif',  'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
 
 function comecarJogo() {
     let condicao = 0;
@@ -20,11 +21,19 @@ comecarJogo();
 
 function colocarCartas() {
     const main = document.querySelector("main");
-    const cartaTras = `<div class="carta-tras">
-    <img src="gifs-e-img/front.png" alt="Papagaio Carta"/>
+    const cartaTras = `
+    <div class="carta">
+        <div class="carta-frente face">
+            <img src="gifs-e-img/front.png" alt="Papagaio carta"/>
+        </div>
+        <div class="carta-tras face">
+            <img src="gifs-e-img/${gifs[0]}" alt="Papagaio gif"/>
+        </div>
     </div>`;
 
     for(i = 0; i < qtdCartas ; i++) {
         main.innerHTML = main.innerHTML + cartaTras;
     }
 }
+
+function virarCarta() {}
